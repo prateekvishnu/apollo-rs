af11b1894dffb9421e8e1e771f9270ef0a994a23
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2022-02-01 11:57:16 +0100

    add reproductible issue for cargo-fuzz
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- fuzz/fuzz_targets/parser.rs
+++ fuzz/fuzz_targets/parser.rs
@@ -1,10 +1,12 @@
 #![no_main]
 use apollo_parser::Parser;
 use apollo_rs_fuzz::generate_valid_document;
-use libfuzzer_sys::fuzz_target;
+use libfuzzer_sys::{arbitrary::Unstructured, fuzz_target};
 use std::panic;
 
 fuzz_target!(|data: &[u8]| {
+    let mut u = Unstructured::new(data);
+    u.int_in_range(0..=1000).unwrap();
     let doc_generated = generate_valid_document(data).unwrap();
 
     let parser = panic::catch_unwind(|| Parser::new(&doc_generated));
